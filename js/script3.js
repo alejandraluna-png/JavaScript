5d7407c023ec2fbbe0c673c9cde6aefc7fdb3813
alejandraluna-png <lunamunozp@gmail.com>
2021-05-05 17:49:49 -0500

    parte 2 de JavaScript

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+function listaEst() {
+    let estudiante = document.getElementById("respuesta").value
+    
+    //condiconal
+    if (estudiante === "presente"){ 
+    alert ("El estudiante esta presente")
+    }
+    else {
+    alert ("El estudiante no esta presente")
+    }
+}

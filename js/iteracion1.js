d7686bab1262fb1ffd70911f97478cbea52e6d07
alejandraluna-png <lunamunozp@gmail.com>
2021-05-11 16:50:02 -0500

    Impresion de JavaScript

--- js/iteracion1.js
+++ js/iteracion1.js
@@ -1,6 +1,5 @@
-for (i=1; i<=10; i=i+1){
-    let card = `<div class="m-3 card" style="width: 18rem;">
-      <table class="table">
+for (i=0; i<=5; i=i+1){
+    let card = `<table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -11,30 +10,11 @@ for (i=1; i<=10; i=i+1){
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">2</th>
+                    <th scope="row">${i}</th>
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
                 </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">6</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <th scope="row">8</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                    
             </tbody>
       </table>`
   document.getElementById("tabla-enumerada").innerHTML += card;
